05d57cd3dcc2cba6e202b3e4948755535928c1a4
hamzaalvi1 <hamzaalvi224@gmail.com>
2022-02-07 13:02:55 +0500

    create structure of olx-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.scss';
+import Header from "./Components/Header"
+// import Footer from "./Components/Footer"
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
